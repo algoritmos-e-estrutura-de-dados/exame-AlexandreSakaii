c53ae972627fbad3d7772d9cd110a0a79378b367
Alexandre Sakai <108962012+AlexandreSakaii@users.noreply.github.com>
2022-10-04 17:00:14 -0300

    exame

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,9 +1,22 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+    joao = []
+    maria = []
 
 
+    for figurinha_maria in figurinhas_da_maria:
+        if (figurinhas_da_maria in figurinhas_do_joao):
+            continue
+        if (figurinha_maria in maria or figurinha_maria in joao):
+            continue
+        for figurinha_joao in figurinhas_do_joao:
+            if (figurinha_joao in figurinha_maria):
+                continue
+            if (figurinha_joao in joao or figurinha_joao in maria):
+                continue
+            maria.append(figurinha_maria)
+            joao.append(figurinha_joao)
+            break
+        return len(maria)
 
 if __name__ == '__main__':
     A, B = input().split(' ')
